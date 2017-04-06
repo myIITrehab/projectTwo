7414d7d86f884330fa5782d89d1b456d1cbb9ba8
kiskisiit1131 <kiskisakvile@gmail.com>
2017-04-05 23:49:34 -0500

    adjusted js further (#10)
    * added in project two info and my group member info
    
    * created skeleton html file
    
    * added in logo for page and adjusted h3 header to say welcome to myiit
    
    * adjusted js further and centered text for mobile design

--- site.js
+++ site.js
@@ -1,6 +1,13 @@
-
   $("document").ready(function(){
-    $("#logintrouble").click(function(){
+    $("#logintrouble").click(function(event){
+      event.preventDefault();
       $("#logintroublepar").toggle();
+    });
+  });
+ 
+   $("document").ready(function(){
+    $("#supportbutton").click(function(event){
+      event.preventDefault();
+      $("#support").toggle();
     });
   });
