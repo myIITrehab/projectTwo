e1e21cf218a6e05cd5e9437cc1be71e5da85005b
Shameemah Fuseini-Codjoe <sfuseini@hawk.iit.edu>
2017-04-10 14:26:57 -0500

    Styling changes, HTML refactoring and JavaScript functionality (#11)
    * Included my strengths
    
    * Added more to skeletal page, notes are below! (#6)
    
    * added in project two info and my group member info
    
    * created skeleton html file
    
    * added in new background image, logo for top, and adjuste h3 tag so page says welcome to myiit (#7)
    
    * added in project two info and my group member info
    
    * created skeleton html file
    
    * added in logo for page and adjusted h3 header to say welcome to myiit
    
    * javascript beginning (#8)
    
    * Added my name and strengths to the README.md file
    
    * added a toggle button with the JavaScript needed to operate it
    
    * Correcting merge errors (#9)
    
    * Added my name and strengths to the README.md file
    
    * added a toggle button with the JavaScript needed to operate it
    
    * corrected merge errors
    
    * adjusted js further (#10)
    
    * added in project two info and my group member info
    
    * created skeleton html file
    
    * added in logo for page and adjusted h3 header to say welcome to myiit
    
    * adjusted js further and centered text for mobile design
    
    * Included image directory and moved all photos to img directory
    
    * Moved screen.css to css directory
    
    * Moved site.js to js directory
    
    * Changed path to screen.css
    
    * Changed path to logo
    
    * Changed path to background image
    
    * Moved logo into header
    
    * Refactored code: removed <p> tags and fixed indentation
    
    * Included baseholdit
    
    * Included div with id page
    
    * Included font with webfont loader
    
    *  resized text to fit baseline groid and selected modular scale. Also changed font type
    
    * Included form name and id
    
    * Changed email-active to login-active
    
    * Changed path for js
    
    * Included functinality for inputs
    
    * Made changes to color and styling of form
    
    * Changed styling for form input elements
    
    * Removed some content and added forgot password link
    
    * Included id for fotgot password link
    
    * Included styling for forgot password link
    
    * Included css styling for label when selected, and removed toggle funtions
    
    * Included support content
    
    * Included classes and ids for support
    
    * Included toggle functions for support info
    
    * Renamed IDs and classes for support section
    
    * Refactored code and created generic function for toggle
    
    * Included styling for support links
    
    * Corrected class name

---
+++ js/site.js
@@ -0,0 +1,93 @@
+$.noConflict();
+
+// jQuery 3.x-style ready event and locally scoped $
+jQuery(function($) {
+
+ //username variables
+  var usernameId = '#username';
+  var usernameLabel = '#input-username label';
+  var typeText = 'input[type="text"]';
+  
+  //password variables
+  var passwordId = '#password';
+  var passwordLabel = '#input-password label';
+  var typePassword = 'input[type="password"]';
+  
+  //toggle element variables
+  var logInTroubleId = '#login-trouble';
+  var logInTroubleInfoId = '#login-trouble-info';
+  var contactId = '#contact';
+  var contactInfoClass = '.contact-info';
+  
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+  
+  //Function Calls
+  addClassFocus(usernameId, usernameLabel, typeText);
+  addClassFocus(passwordId, passwordLabel, typePassword);
+  addClassClick('h1', usernameLabel, usernameId);
+  removeClassBlur(usernameId, usernameLabel, typeText);
+  removeClassBlur(passwordId, passwordLabel, typePassword);
+  hideAndToggle(logInTroubleId, logInTroubleInfoId)
+  hideAndToggle(contactId, contactInfoClass)
+  resetForms();
+  
+  //validate input
+  $('#username, #password').on('keyup focus blur', function() {
+    var currentUsernameVal = $('#username').val();
+    var currentPassVal = $('#password').val();
+    if (currentUsernameVal.length !==0 && currentPassVal.length !== 0) {
+      $('#submit').addClass('active');
+    } else {
+      $('#submit').removeClass('active');
+    }
+  });
+  
+  //Form submit action
+  $( '#login-form' ).submit(function(event) {
+    event.preventDefault();
+  });
+
+	//function to hide and toggle text
+	function hideAndToggle(elementId, toggleElementId) {
+		$(toggleElementId).hide();
+    $(elementId).on('click', function(event){
+    	event.preventDefault();
+    	$(toggleElementId).toggle();
+  	});
+  }
+  
+  //function for addCLass/focus
+  function addClassFocus(element, impactedElement, affectedElement) {
+    $(element).on('focus', function() {
+      $(impactedElement).addClass('login-active');
+      $(affectedElement).addClass('login-active');
+      $(impactedElement).css("color", "grey");
+    });
+  }
+  
+  //function for addClass/click
+  function addClassClick(element, impactedElement, affectedElement) {
+    $(element).on('click', function() {
+      $(impactedElement).addClass('login-active');
+      $(affectedElement).focus();
+      $(impactedElement).css("color", "grey");
+    });
+  }
+  
+  //function for removeClass/blur
+  function removeClassBlur(element, impactedElement, affectedElement) {
+    $(element).on('blur', function() {
+      if($(element).val().length === 0) {
+        $(impactedElement).removeClass('login-active');
+        $(affectedElement).removeClass('login-active');
+        $(impactedElement).css("color", "black");
+      }
+    });
+  }
+  
+  function resetForms() {
+    document.forms['login-form'].reset();
+  }
+});  
+  
